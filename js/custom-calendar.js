3514a303d60fbc196c19faa35b8a06c3cf0bc07b
unknown <oswaldo.jimenezhidalgo@gmail.com>
2017-03-23 16:34:23 -0400

    Fixed footer and html syntax errors

--- js/custom-calendar.js
+++ js/custom-calendar.js
@@ -2,11 +2,16 @@
     "use strict";
 
     var eventsJs = [{
-        "title" : "Feria Quesos Curacautín",
-        "start" : "2017-03-24 10:20:00",
+        "title" : "Fiesta de la Vendimia: Venta de Quesos Curacautín",
+        "start" : "2017-03-23 10:20:00",
         "end" : "2017-03-26 11:00:00",
         "allDay" : true
-     }];
+     },{
+         "title" : "Feria Arte y Sabores de La Araucanía: Venta de Quesos Curacautín",
+         "start" : "2017-03-27 10:20:00",
+         "end" : "2017-03-28 11:00:00",
+         "allDay" : true
+      }];
     $(document).ready(function() {
 
         // page is now ready, initialize the calendar...
